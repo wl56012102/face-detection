42bb31c6cd1f3179f3ead9a3de099d3bc77de589
madbob <735382689@qq.com>
2017-10-15 14:19:52 +0800

    add web-stream

---
+++ web/web-stream.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template, Response
+import cv2
+
+
+class VideoCamera(object):
+    def __init__(self):
+        # 通过opencv获取实时视频流
+        self.video = cv2.VideoCapture(0)
+
+    def __del__(self):
+        self.video.release()
+
+    def get_face(self,image):
+        face_cascade = cv2.CascadeClassifier('../data/haarcascades/haarcascade_frontalface_default.xml')
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+        return faces
+
+    def get_frame(self):
+        success, image = self.video.read()
+        faces = self.get_face(image)
+        for(x,y,w,h) in faces:
+            cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            roi_color = image[y:y + h, x:x + w]
+
+        # 因为opencv读取的图片并非jpeg格式，因此要用motion JPEG模式需要先将图片转码成jpg格式图片
+        ret, jpeg = cv2.imencode('.jpg', image)
+        return jpeg.tobytes()
+
+
+app = Flask(__name__)
+
+
+@app.route('/')  # 主页
+def index():
+    # jinja2模板，具体格式保存在index.html文件中
+    return render_template('index.html')
+
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        # 使用generator函数输出视频流， 每次请求输出的content类型是image/jpeg
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+
+@app.route('/video_feed')  # 这个地址返回视频流响应
+def video_feed():
+    return Response(gen(VideoCamera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True, port = 5000)
