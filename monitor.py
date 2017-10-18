c467b8c7e2d549383a72e98d0495a3656d4ab743
madbob <735382689@qq.com>
2017-10-19 03:25:36 +0800

    add monitor

--- monitor.py
+++ monitor.py
@@ -1,8 +1,7 @@
 import os
 import cv2
-from imutils.video import VideoStream
 import time
-
+from imutils.video import VideoStream
 
 def get_face(img):
     face_cascade = cv2.CascadeClassifier('./data/haarcascades/haarcascade_frontalface_default.xml')
@@ -12,19 +11,22 @@ def get_face(img):
 
 if __name__ == '__main__':
     cascadePath = "./data/haarcascades/haarcascade_profileface.xml"
-    image_paths = [os.path.join('./data/face-data', f) for f in os.listdir('./data/face-data')]
     faceCascade = cv2.CascadeClassifier(cascadePath)
+    image_paths = [os.path.join('./data/face-data', f) for f in os.listdir('./data/face-data')]
     recognizer = cv2.face.createLBPHFaceRecognizer()
     recognizer.load('model.yaml')
     face_name=''
     camera = cv2.VideoCapture(0)
-    vs = VideoStream(0).start()
+    vs =VideoStream(0).start()
     time.sleep(1)
     fourcc = cv2.VideoWriter_fourcc(*"MJPG")
-    writer = None
-    writer = cv2.VideoWriter('output1.avi', fourcc, 15, (640, 480), 1)
+    writer=None
+    writer = cv2.VideoWriter('output.avi', fourcc, 15, (640, 480), 1)
     while True:
         ret ,img = camera.read()
+        now = time.strftime("%Y-%m-%d %H:%M:%S")
+
+        cv2.putText(img, str(now), (20, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
         faces = get_face(img)
         for (x, y, w, h) in faces:
             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -35,14 +37,14 @@ if __name__ == '__main__':
                 cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
                 face_name='unknow'
                 cv2.putText(img, face_name, (x, y - 20), cv2.FONT_HERSHEY_SIMPLEX, 1,(0, 0, 255), 1)
+                output = img
+                writer.write(output)
             else:
                 for image_path in image_paths:
                     if (os.path.split(image_path)[1].split('.')[0] == str(a)):
                         face_name=os.path.split(image_path)[1].split('.')[1]
                 cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
                 cv2.putText(img,str(a)+" "+face_name+" "+str(int(b)),(x,y-20),cv2.FONT_HERSHEY_SIMPLEX,1,(255,0,0),1)
-        output = img
-        writer.write(output)
         cv2.imshow("frame", img)
         key = cv2.waitKey(5)&0xff
         if key ==ord("q"):
