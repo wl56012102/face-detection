1001c20eabd0475de06eca8eb0b59dc61ac3d5fd
madbob <735382689@qq.com>
2017-10-19 02:47:50 +0800

    add get_video

--- get_video.py
+++ get_video.py
@@ -1,25 +1,30 @@
 import cv2
 from imutils.video import VideoStream
 import imutils
-
+import time
 
 def main():
     camera = cv2.VideoCapture(0)
-    fourcc = cv2.VideoWriter_fourcc(*'X264')
-    vs =VideoStream()
-    cv2.dnn.
-
+    vs =VideoStream(0).start()
+    time.sleep(2)
+    fourcc = cv2.VideoWriter_fourcc(*"MJPG")
+    writer=None
+    writer = cv2.VideoWriter('output.avi', fourcc, 15, (640, 480), 1)
     while True:
         ret, frame = camera.read()
-        cv2.imshow("frame", frame)
-        frame = cv2.flip(frame, 1)
-        out.write(frame)
 
+        frame = cv2.flip(frame, 1)
+        now = time.strftime("%Y-%m-%d %H:%M:%S")
+        cv2.putText(frame, str(now), (20, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+        output =frame
+        writer.write(output)
+        cv2.imshow("frame", frame)
         key = cv2.waitKey(5) & 0xff
         if key == ord("q"):
             break
     camera.release()
-    out.release()
+    vs.stop()
+    writer.release()
     cv2.destroyAllWindows()
 
 
